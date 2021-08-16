31f2c8dd1cbb4f3d98bfe7cf063eab559c6f0a55
Christopher Banks <christopherjbanks1@gmail.com>
2021-08-16 16:07:31 -0400

    free inverse matrix and yorai pointer

--- src/modules/src/controller_sam_yorai.c
+++ src/modules/src/controller_sam_yorai.c
@@ -122,6 +122,7 @@ float ** matinv_4d(float matrix_in[ROWS][COLUMNS]){
     //create mat_inv array of pointers
     float ** mat_inv;
     mat_inv = malloc(sizeof(float*) * ROWS);
+
     for (int i = 0; i < ROWS; i++){
         mat_inv[i] = malloc(sizeof(float*) * COLUMNS);
     }
@@ -979,6 +980,14 @@ void controllerSamYorai(control_t* control, setpoint_t* setpoint,
     //control->roll = (int16_t)(u_new[1]);
     //control->pitch =(int16_t)(u_new[2]);
     //control->yaw = (int16_t)(u_new[3]);
+
+    //free matrix inv
+    for (int i =0; i <3; i++){
+        free(Jac_inv[i]);
+    }
+    free(Jac_inv);
+    free(yorai_row_pointer);
+
 
     DEBUG_PRINT("UPDATED THRUST: %f\n", (double) u_new[0]);
     DEBUG_PRINT("UPDATED ROLL: %f\n", (double) u_new[1]);
