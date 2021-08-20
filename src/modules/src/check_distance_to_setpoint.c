33d5c6a87fe9dbfb0d205f43717d66c6a39dcf58
Chris <cbanks34@gatech.edu>
2021-08-19 20:57:44 -0400

    distance checking function

--- src/modules/src/check_distance_to_setpoint.c
+++ src/modules/src/check_distance_to_setpoint.c
@@ -1,4 +1,25 @@
 //
 // Created by quadcopteruser on 8/19/21.
 //
+#include "check_distance_to_setpoint.h"
+#include "math3d.h"
+#include "debug.h"
 
+
+
+bool check_distance_to_setpoint(state_t *state_cf, setpoint_t *setpoint){
+    struct vec setpointPos = mkvec(setpoint->position.x, setpoint->position.y, setpoint->position.z);
+    struct vec statePos = mkvec(state_cf->position.x, state_cf->position.y, state_cf->position.z);
+
+    struct vec r_error = vsub(setpointPos, statePos);
+
+    double distance = sqrt(pow((double) r_error.x, 2) + pow((double) r_error.y, 2) +
+            pow((double) r_error.z, 2));
+
+
+    //DEBUG_PRINT("RUN MELLINGER -- CHECK DISTANCE: %f \n", distance);
+    if (distance <= 0.01 && setpoint->position.z != 0)
+        return true;
+    else
+        return false;
+}
