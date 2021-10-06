f785516c074c5a387c1c6c662a5ae8fa383ea942
Chris <cbanks34@gatech.edu>
2021-10-06 19:24:45 -0400

    updated stabilizer
    switch to sam yorai when thrust >0

--- src/modules/src/stabilizer.c
+++ src/modules/src/stabilizer.c
@@ -252,7 +252,7 @@ static void stabilizerTask(void* param)
                 estimatorType = getStateEstimator();
             }
             // allow to update controller dynamically
-            if (check_distance_to_setpoint(&state, &setpoint) == true && controllerType == ControllerTypeMellinger && tick >= 25000){
+            if (setpoint.thrust > 0 && controllerType == ControllerTypeMellinger){
                 DEBUG_PRINT("Switch to Sam-Yorai Controller.\n");
                 controllerType = ControllerTypeSamYorai;
             }
